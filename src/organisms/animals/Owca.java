9391a5c419e9a373cff61ae7f6cecdfbc6368642
notJachu <jasiustasiu50@interia.pl>
2024-05-17 22:16:34 +0200

    Implemented Owca

---
+++ src/organisms/animals/Owca.java
@@ -0,0 +1,11 @@
+package organisms.animals;
+
+public class Owca extends Animal{
+    public Owca() {
+        super(4, 4, 0, null);
+    }
+
+    public Owca(int power, int initiative, int age, java.awt.Point position) {
+        super(power, initiative, age, position);
+    }
+}
