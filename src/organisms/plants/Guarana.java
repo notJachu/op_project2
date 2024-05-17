10257904197e482acf2d66a34fa687bbad36f15c
notJachu <jasiustasiu50@interia.pl>
2024-05-17 22:17:04 +0200

    Implemented Guarana

---
+++ src/organisms/plants/Guarana.java
@@ -0,0 +1,19 @@
+package organisms.plants;
+import organisms.Creature;
+
+public class Guarana extends Plant{
+
+    public Guarana() {
+        super(0, 0, 0, null);
+    }
+
+    public Guarana(int power, int initiative, int age, java.awt.Point position) {
+        super(power, initiative, age, position);
+    }
+
+   @Override
+    public boolean collision(Creature other) {
+        other.set_power(other.get_power() + 3);
+        return true;
+    }
+}
