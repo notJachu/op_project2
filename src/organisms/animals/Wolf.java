f4754239ff3ed883015c5295783f6bd0988c433c
notJachu <jasiustasiu50@interia.pl>
2024-05-18 20:47:40 +0200

    implemented wolf

--- src/organisms/animals/Wolf.java
+++ src/organisms/animals/Wolf.java
@@ -3,5 +3,12 @@ import organisms.Creature;
 
 public class Wolf extends Creature {
 
+    public Wolf() {
+        super(9, 5, 0, null);
+    }
+
+    public Wolf(int power, int initiative, int age, java.awt.Point position) {
+        super(power, initiative, age, position);
+    }
 
 }
